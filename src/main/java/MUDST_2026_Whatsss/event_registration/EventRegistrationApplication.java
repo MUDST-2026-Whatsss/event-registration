23b1f509df6a587348655a464a13e6357f2ca4c9
aiko.amonrat@gmail.com <aiko.amonrat@gmail.com>
2026-08-09 12:14:17 +0700

    Setup SpringBoot API with HelloWorld

---
+++ src/main/java/MUDST_2026_Whatsss/event_registration/EventRegistrationApplication.java
@@ -0,0 +1,13 @@
+package MUDST_2026_Whatsss.event_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationApplication.class, args);
+	}
+
+}
